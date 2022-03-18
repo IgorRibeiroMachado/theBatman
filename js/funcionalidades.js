1bdf2a04883b6f880693e0328e71f16b90b981b1
IgorRibeiroMachado <igoribeirom@gmail.com>
2022-03-18 19:40:38 -0300

    Adicionei novas funcionalidades

--- js/funcionalidades.js
+++ js/funcionalidades.js
@@ -15,18 +15,39 @@ const inicializarSection = _ => {
     window.onload = function() {
         const section = document.querySelector('section')
         const inicio = document.querySelector('#inicio')
-        const musica = document.querySelector('audio')
-
+        
         // Inicializa a Seção
         fetch(inicio.href)
             .then(resp => resp.text())
             .then(html => section.innerHTML = html)
-
     }
 }
 
+const tocarMusica = _ => {
+    const musica = document.querySelector('audio')
+    const botaoMusica = document.querySelector('#botaoMusica')
+    
+    botaoMusica.onclick = e => {
+    
+        botaoPlay = "image/playButton.ico"
+        botaoPause = "image/pauseButton.ico"
+
+        botaoMusica.src = botaoPlay
+
+        if (botaoMusica.getAttribute('play') == 'true') {
+            botaoMusica.src = botaoPause
+            botaoMusica.setAttribute('play', 'false')
+            musica.play()
+        } else {
+            botaoMusica.src = botaoPlay
+            botaoMusica.setAttribute('play', 'true')
+            musica.pause()
+        }
+    }
+}
 
 inicializarSection()
+tocarMusica()
 trocarPagina()
 
 
